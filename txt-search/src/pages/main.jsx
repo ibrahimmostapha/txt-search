3db1e9bab0ccb1f26fce5759fb4acf8925b2b29c
Ibrahim Mostapha <116353800+ibrahimmostapha@users.noreply.github.com>
2024-08-17 01:46:29 +0300

    add-search-function

--- txt-search/src/pages/main.jsx
+++ txt-search/src/pages/main.jsx
@@ -1,7 +1,37 @@
 import './style.css'
+import React, { useState, useEffect } from 'react';
 
-function SearchPage(){
+function SearchPage() {
 
+  const [searchedItem, setSearchedItem] = useState(''); // inital state of search
+
+  function searchInput(e) { // get the search from input
+    setSearchedItem(e.target.value);
+  }
+
+  function highlightText() {
+    const search = searchedItem.trim().toLowerCase(); // make it case-insensitive
+    if (search.length < 2) { 
+      return;
+    }
+    const content = document.getElementById('content'); // get the element that has "content" as ID
+    if (content) {
+      const paragraphs = content.getElementsByTagName('p'); // get paragraphs from it
+      for (let i = 0; i < paragraphs.length; i++) { // loop on paragraphs
+        const paragraph = paragraphs[i];
+        const text = paragraph.textContent || '';
+        const lowerText = text.toLowerCase(); // make it case-insensitive
+        const reg = new RegExp(`(${search})`, 'gi'); // regular expression to replace matched items later
+        if (lowerText.includes(search)) { // find matched items using includes() function
+          // add "highlight" class to matched to get special css
+          paragraph.innerHTML = text.replace(reg,'<span class="highlight">$1</span>');
+        }
+      }
+
+    }
+  }
+
+  useEffect(function (){highlightText();} , [searchedItem]); // triger the seach every time the input is changed
 
   return (
     <div>
@@ -10,6 +40,8 @@ function SearchPage(){
           className="search-input"
           type="text"
           placeholder="Search"
+          value={searchedItem}
+          onInput={searchInput}
         />
       </div>
       
